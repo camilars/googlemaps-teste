762bdb9990e5a2d55127fe3dcc313d0fdbbe7a82
camilars <camila.rs122@gmail.com>
2018-09-21 14:33:25 -0300

    iniciando protejo

---
+++ dados.js
@@ -0,0 +1,5 @@
+var filiais = 
+[{estado: "Pernambuco", cidade: "Recife", nome: "X", latitude: "-8.0360", longitude: "-34.9800"},
+{estado: "Paraíba", cidade: "João Pessoa", nome: "Y",latitude: "-8.0360", longitude: "-34.9800"},
+{estado: "São Paulo", cidade: "São Paulo (Capital)", nome: "Z", latitude: "-8.0360", longitude: "-34.9800"},
+{estado: "São Paulo", cidade: "São Paulo (Interior)", nome: "W", latitude: "-8.0360", longitude: "-34.9800"}];
